6f55f53659ca023f50db941a0ce38c05dc4b4a51
Mark Baxter <mark@baxter.codes>
2018-12-02 16:27:33 +0000

    CSRF_Vulnerability

---
+++ SSD_Web_App/Controllers/UploadDashboard.cs
@@ -0,0 +1,26 @@
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SSD_Web_App.Controllers
+{
+    [Authorize]
+    public class UploadDashboardController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        // From Form attribute: https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        public IActionResult Analyse([FromForm]string code)
+        {
+            ViewData["Response"] = code;
+
+            return View();
+        }
+    }
+}
