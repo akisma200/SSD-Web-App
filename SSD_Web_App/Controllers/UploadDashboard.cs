04ab117072f2af4d6cc20c30d04021907898f92d
Mark Baxter <mark@baxter.codes>
2018-12-05 00:26:17 +0000

    stasfsaf

--- SSD_Web_App/Controllers/UploadDashboard.cs
+++ SSD_Web_App/Controllers/UploadDashboard.cs
@@ -7,20 +7,17 @@ namespace SSD_Web_App.Controllers
     [Authorize]
     public class UploadDashboardController : Controller
     {
-
         public IActionResult Index()
         {
             return View();
         }
 
-
         [HttpPost]
-        [ValidateAntiForgeryToken]
+        // Andy - Make sure AFT is checked to prevent CSRF
+        //[ValidateAntiForgeryToken]
         // From Form attribute: https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
         public IActionResult Analyse([FromForm]string code)
         {
-            
-
             ViewData["Response"] = code;
 
             return View();
